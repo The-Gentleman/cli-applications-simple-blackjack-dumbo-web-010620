69a3baac9491f7dddd54f65eb10dcd21b3418569
Zeshan Raja <zraja4402@gmail.com>
2020-01-04 03:14:12 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,54 @@
+require "pry"
+
 def welcome
-  # code #welcome here
+ puts 'Welcome to the Blackjack Table'
 end
 
 def deal_card
-  # code #deal_card here
+  return rand(1...12)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+ puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+ puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+sum = deal_card + deal_card
+display_card_total(sum)
+sum 
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+prompt_user
+user_input = get_user_input
+
+if user_input != 'h' && user_input != 's'
+  invalid_command
+  prompt_user
+  user_input = get_user_input
+end 
+
+if user_input == 'h'
+ card_total += deal_card
+end 
+
+card_total
 end
 
 def invalid_command
-  # code invalid_command here
+ puts 'Please enter a valid command'
 end
 
 #####################################################
@@ -39,6 +56,20 @@ end
 #####################################################
 
 def runner
-  # code runner here
+welcome
+initial_round
+hit?(initial_round)
+prompt_user
+# binding.pry
+get_user_input
+user_input = get_user_input
+new_total = display_card_total(card_total)
+
+if user_input == 's'
+ prompt_user
+elsif user_input == 'h'
+ new_total
+end 
+
 end
     
